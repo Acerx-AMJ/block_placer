d2afeab8edb5a2a9ccd5913070ec4a4638c8380a
Acerx-AMJ <ajm.acerx@gmail.com>
2025-10-09 22:37:42 +0300

    Added piece rotation and clearing. Added an I piece.

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 
 // Constants
 
-constexpr Vector2 screen {540, 636}; 
-constexpr Vector2 tile_size {10, 20};
+constexpr Vector2 screen {600, 700}; 
+constexpr Vector2 tile_size {12, 22};
 constexpr float tile_scale = .5f;
 
 // Tiles
@@ -22,10 +22,17 @@ struct Tile {
     Color color;
 };
 
-Tile tiles[(unsigned)tile_size.y][(unsigned)tile_size.x];
+std::vector<std::vector<Tile>> tiles;
 
 std::vector<Piece> pieces {
-    {{{1, 1}, {1, 1}}}, {{{1, 1, 1}, {0, 0, 1}}}
+    {{{1, 1}, {1, 1}}},
+    {{{0, 0, 1}, {1, 1, 1}, {0, 0, 0}}},
+    {{{1, 0, 0}, {1, 1, 1}, {0, 0, 0}}},
+    {{{0, 1, 1}, {1, 1, 0}, {0, 0, 0}}},
+    {{{1, 1, 0}, {0, 1, 1}, {0, 0, 0}}},
+    {{{0, 1, 0}, {1, 1, 1}, {0, 0, 0}}},
+    {{{0, 0, 0}, {1, 1, 1}, {0, 0, 0}}},
+    {{{0, 0, 0, 0}, {1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}}},
 };
 
 std::vector<Color> colors {
@@ -36,8 +43,8 @@ std::vector<Color> colors {
 // Draw functions
 
 void clear(Piece& piece, int x, int y) {
-    for (int yy = y; yy < tile_size.y and yy < y + piece.tiles.size(); ++yy) {
-        for (int xx = x; xx < tile_size.x and xx < x + piece.tiles[0].size(); ++xx) {
+    for (int yy = y; yy < tile_size.y and yy < y + (int)piece.tiles.size(); ++yy) {
+        for (int xx = x; xx < tile_size.x and xx < x + (int)piece.tiles[0].size(); ++xx) {
             if (piece.tiles[yy - y][xx - x] and not tiles[yy][xx].border) {
                 tiles[yy][xx].on = false;
             }
@@ -46,8 +53,8 @@ void clear(Piece& piece, int x, int y) {
 }
 
 void draw(Piece& piece, int x, int y, const Color& color) {
-    for (int yy = y; yy < tile_size.y and yy < y + piece.tiles.size(); ++yy) {
-        for (int xx = x; xx < tile_size.x and xx < x + piece.tiles[0].size(); ++xx) {
+    for (int yy = y; yy < tile_size.y and yy < y + (int)piece.tiles.size(); ++yy) {
+        for (int xx = x; xx < tile_size.x and xx < x + (int)piece.tiles[0].size(); ++xx) {
             if (piece.tiles[yy - y][xx - x] and not tiles[yy][xx].border) {
                 tiles[yy][xx].on = true;
                 tiles[yy][xx].color = color;
@@ -61,10 +68,11 @@ void draw(Piece& piece, int x, int y, const Color& color) {
 constexpr int LEFT = 0;
 constexpr int RIGHT = 1;
 constexpr int DOWN = 2;
+constexpr int CURRENT = 3;
 
 bool can_move(Piece& piece, int x, int y, int direction) {
-    for (int yy = y; yy < tile_size.y and yy < y + piece.tiles.size(); ++yy) {
-        for (int xx = x; xx < tile_size.x and xx < x + piece.tiles[0].size(); ++xx) {
+    for (int yy = y; yy < tile_size.y and yy < y + (int)piece.tiles.size(); ++yy) {
+        for (int xx = x; xx < tile_size.x and xx < x + (int)piece.tiles.size(); ++xx) {
             if (not piece.tiles[yy - y][xx - x]) {
                 continue;
             }
@@ -75,10 +83,61 @@ bool can_move(Piece& piece, int x, int y, int direction) {
                 return false;
             } else if (direction == DOWN and (yy + 1 >= tile_size.y or tiles[yy + 1][xx].on)) {
                 return false;
+            } else if (direction == CURRENT and tiles[yy][xx].on) {
+                return false;
             }
         }
     }
     return true;
+}
+
+bool rotate(Piece& piece, int x, int y) {
+    Piece new_piece = piece;
+    
+    for (int y = 0; y < piece.tiles.size(); ++y) {
+        for (int x = 0; x < piece.tiles[0].size(); ++x) {
+            new_piece.tiles[y][x] = piece.tiles[piece.tiles.size() - x - 1][y];
+        }
+    }
+
+    bool can_rotate = can_move(new_piece, x, y, CURRENT);
+    if (can_rotate) {
+        piece = new_piece;
+    }
+    return can_rotate;
+}
+
+// Clear
+
+void clear_cleared_rows() {
+    std::vector<int> cleared_rows;
+    
+    for (int y = 1; y < tile_size.y - 1; ++y) {
+        for (int x = 1; x < tile_size.x - 1; ++x) {
+            if (not tiles[y][x].on) {
+                break;
+            }
+
+            if (x == tile_size.x - 2) {
+                cleared_rows.push_back(y);
+                for (int nx = 1; nx < tile_size.x - 1; ++nx) {
+                    tiles[y][nx].on = false;
+                }
+            }
+        }
+    }
+
+    if (cleared_rows.empty()) {
+        return;
+    }
+
+    for (const auto& yy : cleared_rows) {
+        for (int y = yy; y >= 2; --y) {
+            for (int x = 1; x < tile_size.x - 1; ++x) {
+                tiles[y][x] = tiles[y - 1][x];
+            }
+        }
+    }
 }
 
 // Other helper functions
@@ -105,13 +164,16 @@ int main() {
     Texture tile_texture = LoadTexture("assets/tile.png");
 
     for (int y = 0; y < tile_size.y; ++y) {
+        std::vector<Tile> row;
         for (int x = 0; x < tile_size.x; ++x) {
             if (y == 0 or y == tile_size.y - 1 or x == 0 or x == tile_size.x - 1) {
-                tiles[y][x].border = true;
-                tiles[y][x].on = true;
-                tiles[y][x].color = GRAY;
+                Tile tile {true, true, GRAY};
+                row.push_back(tile);
+            } else {
+                row.push_back({});
             }
         }
+        tiles.push_back(row);
     }
 
     Piece piece = get_random_piece();
@@ -127,6 +189,7 @@ int main() {
         // Controls
 
         if (next_piece) {
+            clear_cleared_rows();
             piece = get_random_piece();
             color = get_random_color();
             x = 4;
@@ -136,6 +199,21 @@ int main() {
         }
 
         clear(piece, x, y);
+        if (key_pressed(KEY_UP) or key_pressed(KEY_W)) {
+            bool rotated = rotate(piece, x, y);
+            if (not rotated) {
+                rotated = rotate(piece, x + 1, y);
+                if (rotated) {
+                    x++;
+                } else {
+                    rotated = rotate(piece, x - 1, y);
+                    if (rotated) {
+                        x--;
+                    }
+                }
+            }
+        }
+
         if ((key_pressed(KEY_DOWN) or key_pressed(KEY_S)) and can_move(piece, x, y, DOWN)) {
             y++;
             down_timer = 0.f;
@@ -153,6 +231,7 @@ int main() {
             while (can_move(piece, x, y, DOWN)) {
                 y++;
             }
+            down_timer = down_after;
         }
 
         down_timer += GetFrameTime();
